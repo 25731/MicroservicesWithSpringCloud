9383128c71cf9020345f4f18d94b4710f65a157a
BJanosik <witoldjan153@gmail.com>
2023-06-26 23:38:42 +0200

    feat: Claster eureka server completed

--- Eureka2/src/main/java/com/example/eureka2/Eureka2Application.java
+++ Eureka2/src/main/java/com/example/eureka2/Eureka2Application.java
@@ -2,10 +2,12 @@ package com.example.eureka2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class Eureka2Application {
 
 	public static void main(String[] args) {
